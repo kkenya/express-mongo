e55e274ae762d88af79d90847822762877cdf67c
Kenya Kawatsu <3982ne@gmail.com>
2019-08-06 18:55:12 +0900

    add /api/users PUT

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res, next) => {
   res.send(users)
 })
 
-router.post('/user', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   if (!req.body.user.name && !req.body.user.email) {
     return res.status(422).json({ error: 'invalid' })
   }
@@ -16,4 +16,8 @@ router.post('/user', async (req, res, next) => {
   res.send('respond with a resource')
 })
 
+router.put('/user', async (req, res, next) => {
+  const user = await User.find(req.body.id)
+
+})
 module.exports = router
